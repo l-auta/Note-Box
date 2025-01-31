f16ce5bcb7bd9489b0cfe054d8564fae7dffb620
lisa <lisaauta018@gmail.com>
2025-01-31 21:23:01 +0300

    changed the vite config file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // Configure Vite to always fallback to index.html for non-file paths
+    historyApiFallback: true,
+  }
 })
