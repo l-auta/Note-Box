0ed3cbb262f9de5c6aa79b50a476c90b2de0296d
lisa <lisaauta018@gmail.com>
2025-02-07 19:22:20 +0300

    removed icon

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, request, flash,  jsonify, redirect, url_for
+from flask import Flask, session, request, flash,  jsonify, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
